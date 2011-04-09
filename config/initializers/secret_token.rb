4c1c545d961e9f5141a5c55d78f6fbb60472525a
Jeff Carley <jeff.carley@gmail.com>
2011-04-09 10:21:32 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Smsposts::Application.config.secret_token = 'ad02f22bd185396cbadc9da8a81a119659d1dd8334c8d93b078318e2bbec2ae91f2736ea2122a71ade07bcb14b9045e0e2fa581738372b69b62f755bc40603b5'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Smsposts::Application.config.session_store :cookie_store, :key => '_smsposts_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Smsposts::Application.config.session_store :active_record_store
